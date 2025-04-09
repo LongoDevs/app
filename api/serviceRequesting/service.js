0dd1a4f909fb527af1c79be74863810d58d5eb7d
K-art-spec <yakakuhle@gmail.com>
2025-04-09 15:58:44 +0200

    service.js

---
+++ api/serviceRequesting/service.js
@@ -0,0 +1,21 @@
+const ServiceRequest = require("./model");
+
+class ServiceRequestService {
+  async createServiceRequest(data) {
+    return await ServiceRequest.create(data);
+  }
+
+  async getServiceRequests() {
+    return await ServiceRequest.find().populate("userId", "name email"); // Show user details
+  }
+
+  async updateServiceRequest(id, updateData) {
+    return await ServiceRequest.findByIdAndUpdate(id, updateData, { new: true });
+  }
+
+  async deleteServiceRequest(id) {
+    return await ServiceRequest.findByIdAndDelete(id);
+  }
+}
+
+module.exports = new ServiceRequestService();
