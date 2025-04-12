d50389e2a6c43cc77fa77ca96ea25cf149039de5
Kgabo Baloyi <73986122+Kgabo71@users.noreply.github.com>
2025-04-12 12:54:12 +0200

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-const { db } = require('../utils/firebase');
+/*const { db } = require('../utils/firebase');
 
 exports.checkRole = (requiredRole) => async (req, res, next) => {
   const { userId } = req.body;
@@ -15,3 +15,25 @@ exports.checkRole = (requiredRole) => async (req, res, next) => {
     res.status(500).json({ message: 'Server error' });
   }
 };
+
+Original code
+*/
+const { admin } = require('../utils/firebase');
+
+exports.authenticateUser = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  const idToken = authHeader.split('Bearer ')[1];
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    req.userId = decodedToken.uid; // Attach UID to request
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
