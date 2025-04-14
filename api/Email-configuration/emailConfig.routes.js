27f82a79aefc58f5be73a4f2d891e99fe700af47
Kgabo Baloyi <73986122+Kgabo71@users.noreply.github.com>
2025-04-14 19:20:25 +0200

    Update emailConfig.routes.js

--- api/Email-configuration/emailConfig.routes.js
+++ api/Email-configuration/emailConfig.routes.js
@@ -4,5 +4,6 @@ const controller = require('./emailConfig.controller');
 
 router.post('/api/admin/email-config', controller.saveEmailConfig);
 router.get('/api/admin/email-config', controller.getEmailConfig);
+router.post('/send-test', sendTestEmail); 
 
 module.exports = router;
