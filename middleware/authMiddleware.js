58b6ce12a801c4898759fd2cba27dfb845cce150
K-art-spec <yakakuhle@gmail.com>
2025-04-16 21:31:02 +0200

    authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -18,6 +18,7 @@ exports.checkRole = (requiredRole) => async (req, res, next) => {
 
 Original 
 */
+
 const { admin } = require('../utils/firebase');
 
 exports.authenticateUser = async (req, res, next) => {
@@ -37,3 +38,20 @@ exports.authenticateUser = async (req, res, next) => {
   }
 };
 
+const jwt = require('jsonwebtoken');
+
+const protect = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith('Bearer ')) {
+    const token = authHeader.split(' ')[1];
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch (err) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+  } else {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+};
